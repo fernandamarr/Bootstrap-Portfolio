57a0b8dda1c363e818079a82a126db75908b60f5
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-01-15 16:15:02 -0500

    add styling

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    // Scroll to top of page on window refresh
+    // window.onbeforeunload = function () {
+    //     window.scrollTo(0, 0);
+    // }
+
+    // Hide page titles
+    // $(".title").hide();
+
+    // Scroll anchors over course of 1 second 
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 700);
+        }
+    });
+
+    $('a[href^="#"]').on('click', function (event) {
+        $(".title").show();
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $(".title").animate({ fontSize: '4em' }, "slow");
+        }
+    });
+
+    // Animate code icon
+    
+
+    $("#code-icon").hover(function() {
+        $(this).addClass('beat');
+        animateCode();
+    })
+
+});
